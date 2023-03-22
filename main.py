2303791d080237708e66cb9e36a9ea237f84a856
C.Zhu <113450928+D1oStar@users.noreply.github.com>
2023-03-23 00:19:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import keras.models
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+
+model_path = 'model/%s'
 
 fashion_mnist = tf.keras.datasets.fashion_mnist
 (mnist_train_images, mnist_train_labels), (mnist_test_images, mnist_test_labels) = fashion_mnist.load_data()
@@ -10,23 +13,30 @@ mnist_class_name = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sand
 
 class tfMnistTrainer:
     def __init__(self, train_images=mnist_train_images, test_images=mnist_test_images,
-                 train_labels=mnist_train_labels, test_labels=mnist_test_labels, class_names=mnist_class_name,
+                 train_labels=mnist_train_labels, test_labels=mnist_test_labels, class_names=None,
                  dense=None, optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                 loss_weight=None):
-        self.class_names = class_names
-        if dense is None:
-            dense = [
-                tf.keras.layers.Flatten(input_shape=(28, 28)),
-                tf.keras.layers.Dense(128, activation='relu'),
-                tf.keras.layers.Dense(10)
-            ]
+                 loss_weight=None, model=None):
 
+        if class_names is None:
+            class_names = mnist_class_name
+
+        if model is not None:
+            self.model = keras.models.load_model(model_path.format(model))
+        else:
+            if dense is None:
+                dense = [
+                    tf.keras.layers.Flatten(input_shape=(28, 28)),
+                    tf.keras.layers.Dense(128, activation='relu'),
+                    tf.keras.layers.Dense(10)
+                ]
+            self.model = tf.keras.Sequential(dense)
+
+        self.class_names = class_names
         self.train_images = train_images / 255.0
         self.test_images = test_images / 255.0
         self.train_labels = train_labels
         self.test_labels = test_labels
 
-        self.model = tf.keras.Sequential(dense)
         self.model.compile(optimizer=optimizer, loss=loss, loss_weights=loss_weight, metrics=['accuracy'])
         self.model.fit(self.train_images, self.train_labels, epochs=10)
         self.probability_model = tf.keras.Sequential([self.model, tf.keras.layers.Softmax()])
@@ -37,6 +47,9 @@ class tfMnistTrainer:
             test_images = self.test_images
         self.predictions = self.probability_model.predict(test_images)
 
+    def model_save(self, name):
+        self.model.save(model_path.format(name))
+
     def evaluate(self, verbose=2, test_images=None, test_labels=None):
         if test_images is None:
             test_images = self.test_images
@@ -46,8 +59,8 @@ class tfMnistTrainer:
         test_loss, test_acc = self.model.evaluate(test_images, test_labels, verbose=verbose)
         print('\nTest accuracy:', test_acc)
 
-    def plot_image(self, i, predictions_array, true_label, img):
-        true_label, img = true_label[i], img[i]
+    def plot_image(self, ind, predictions_array, true_label, img):
+        true_label, img = true_label[ind], img[ind]
         plt.grid(False)
         plt.xticks([])
         plt.yticks([])
@@ -66,8 +79,8 @@ class tfMnistTrainer:
                    color=color)
 
 
-def plot_value_array(i, predictions_array, true_label):
-    true_label = true_label[i]
+def plot_value_array(ind, predictions_array, true_label):
+    true_label = true_label[ind]
     plt.grid(False)
     plt.xticks(range(10))
     plt.yticks([])
